4534a619e83e823473c23ee7efe3741d6d44c7c7
KoichiDevs <alexisken1432@gmail.com>
2023-03-09 00:22:53 +0800

    added more animations

--- src/components/NFTImage.jsx
+++ src/components/NFTImage.jsx
@@ -14,21 +14,24 @@ const NFTImage = ({ i }) => {
     }
 
     return (
-        <motion.div animate={hover ? { rotate: [0, 5, -5, 5, -5, 0] } : { rotate: 0 }} className="bg-picbg border-2 border-mypink h-auto w-[16rem] p-3 mx-auto rounded-xl z-10 relative" onMouseEnter={enter} onMouseLeave={leave}>
-            <div className="h-[14rem] relative">
-                <img src="/dummy.webp" alt="" className="absolute w-full h-full object-cover rounded-xl" />
-            </div>
-            <div className="w-full h-auto">
-                <div className=" flex items-center justify-between mt-2">
-                    <h1 className='font-header text-2xl font-bold ml-2'>PooNomic</h1>
-                    <p className='text-md mr-2'>0 ETH</p>
+        <motion.div initial={{opacity: 0}} whileInView={{opacity: 100, scale: [0.5, 1]}} className="">
+            <motion.div animate={hover ? { rotate: [0, 5, -5, 5, -5, 0] } : { rotate: 0 }} className="bg-picbg border-2 border-mypink h-auto w-[16rem] p-3 mx-auto rounded-xl z-10 relative" onMouseEnter={enter} onMouseLeave={leave}>
+                <div className="h-[14rem] relative">
+                    <img src="/dummy.webp" alt="" className="absolute w-full h-full object-cover rounded-xl" />
+                </div>
+                <div className="w-full h-auto">
+                    <div className=" flex items-center justify-between mt-2">
+                        <h1 className='font-header text-2xl font-bold ml-2'>PooNomic</h1>
+                        <p className='text-md mr-2'>0 ETH</p>
+
+                    </div>
 
                 </div>
 
-            </div>
-
-            <motion.div initial={{ opacity: 0 }} animate={hover ? { opacity: 100 } : {opacity: 0}} className="w-full h-full shadow-nftglow absolute top-0 left-0 rounded-xl"></motion.div>
+                <motion.div initial={{ opacity: 0 }} animate={hover ? { opacity: 100 } : { opacity: 0 }} className="w-full h-full shadow-nftglow absolute top-0 left-0 rounded-xl"></motion.div>
+            </motion.div>
         </motion.div>
+
     )
 }
 
