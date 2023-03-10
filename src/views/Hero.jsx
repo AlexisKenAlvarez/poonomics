11a6249ebdac408d28176f53e833f574bd6258fe
KoichiDevs <alexisken1432@gmail.com>
2023-03-11 00:35:15 +0800

    fixed contrast

--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -74,14 +74,16 @@ const Hero = () => {
                     <motion.div className="mx-auto w-full" style={{ opacity: opacity1 }}>
                         <img src="/topog.webp" alt="Topog" className="absolute h-full right-0 top-0" />
 
-                        <div className="w-2 h-2 absolute bg-[#594798] rounded-full shadow-heroglow1 top-[18rem] left-[15%] z-[1]"></div>
-                        <div className="w-10 h-10 absolute bg-[#6e3465] rounded-full shadow-heroglow2 bottom-[15rem] right-[20%] z-0"></div>
+                        <div className="w-[14rem] h-[14rem] absolute blur-[135px] bg-[#CDA1FF] rounded-full  top-[18rem] left-[15%] z-[1]"></div>
+                        <div className="w-[14rem] h-[14rem] absolute blur-[135px] bg-[#E75F69] rounded-full  bottom-[15rem] right-[20%] z-0"></div>
+
+
 
                         <div className="w-fit px-8 2xl:px-0 mx-auto flex items-center gap-x-0 relative z-10 2xl:gap-x-[8rem] 2xl:max-w-[1500px]">
                             <div className="w-full md:min-w-[30rem] md:text-left text-center">
-                                <motion.h2 initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 0.7, ease: [0.33, 1, 0.68, 1] }} className='text-mypink md:mx-0 mx-auto font-medium '>Be a poonomics</motion.h2>
+                                <motion.h2 initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 0.7, ease: [0.33, 1, 0.68, 1] }} className='text-mypink md:mx-0 mx-auto font-medium '>Be a poonomic</motion.h2>
                                 <motion.h1 initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 0.7, ease: [0.33, 1, 0.68, 1], delay: 0.1 }} className='font-header md:text-5xl 2xl:text-7xl 2xl:max-w-[70rem] font-bold max-w-[33rem] mt-2 md:leading-[3.4rem] md:mx-0 mx-auto text-4xl'>Discover Our Rewards dApp And <span className="w-fit h-fit text-transparent bg-gradient-to-br from-sauce to-mypink bg-clip-text">NFT Collections</span></motion.h1>
-                                <motion.p initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 0.7, ease: [0.33, 1, 0.68, 1], delay: 0.15 }} className='text-mygrey max-w-[33rem] mt-3 md:mx-0 mx-auto'>PooNomics is the Launchpad for PooChain and the first solution that aims to offer a complete set of cryptocurrencies tools that run on PooChain.</motion.p>
+                                <motion.p initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 0.7, ease: [0.33, 1, 0.68, 1], delay: 0.15 }} className='text-[#d3d2d2] max-w-[33rem] mt-3 md:mx-0 mx-auto'>PooNomics is the Launchpad for PooChain and the first solution that aims to offer a complete set of cryptocurrencie tools that run on PooChain.</motion.p>
 
                                 <motion.div initial={{ y: 50, opacity: 0 }} animate={{ y: 0, opacity: 100 }} transition={{ duration: 0.7, ease: [0.33, 1, 0.68, 1], delay: 0.2 }} className="mt-10 flex gap-x-8 items-center md:mx-0 mx-auto w-fit md:flex-row flex-col gap-y-7">
                                     <a href="" target="_blank" rel="noopener noreferrer" className="">
